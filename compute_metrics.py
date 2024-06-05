e237c403ac69b106b58875f39126a3ba5e4243ed
chienhung1519 <chienhung1519@gmail.com>
2024-06-05 15:10:05 +0000

    compute metrics

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,20 +1,28 @@
 from argparse import ArgumentParser
 import json
 import pandas as pd
+import re
+from pathlib import Path
+
+
+TARGETS = [
+    "ALK", "EGFR_18", "EGFR_19", "EGFR_20", "EGFR_21", "Htype", "LV_invasion", "PDL1_immune", 
+    "ROS1", "diagnosis", "operation", "organ", "CK7", "TTF-1", "CK20", "P40"
+]
 
 
 def parse_args():
     parser = ArgumentParser(description="Compute metrics for the model")
-    parser.add_argument("--predictions_file", type=str, help="Path to the predictions")
+    parser.add_argument("--predictions_file", type=str, help="Path to the predictions file")
     parser.add_argument("--outputs_dir", type=str, help="Path to the output file")
+    parser.add_argument("--model_type", type=str, choices=["llama", "t5", "bert"])
     return parser.parse_args()
 
 
 def load_from_jsonl(file_name: str) -> list[dict]:
     def load_json_line(line: str, i: int, file_name: str):
         try:
-            json_line = json.loads(line)
-            return {k: json.loads(v) for k, v in json_line.items()}
+            return json.loads(line)
         except:
             raise ValueError(f"Error in line {i+1}\n{line} of {file_name}")
     with open(file_name, "r") as f:
@@ -22,11 +30,86 @@ def load_from_jsonl(file_name: str) -> list[dict]:
     return data
 
 
-def metrics(labels, preds):
+def extract_llama_labels_and_predictions(results: list[dict]) -> tuple[list[dict], list[dict]]:
+    labels = [json.loads(result["label"]) for result in results]
+    predictions = [json.loads(result["predict"]) for result in results]
+    return labels, predictions
+
+
+def extract_t5_labels_and_predictions(results: list[dict]) -> tuple[list[dict], list[dict]]:
+    labels = [json.loads(result["target_text"]) for result in results]
+    predictions = [json.loads("{"+result["prediction"]+"}") for result in results]
+    return labels, predictions
+
+
+def tags_to_json(tags: list[str], tokens: list[str]) -> dict:
+    json_tags = {}
+    cur_tag = None
+    for tag, token in zip(tags, tokens):
+        if tag.startswith("B-"):
+            tag_name = tag[2:]
+            json_tags[tag_name] = [token]
+            cur_tag = tag_name
+        elif tag.startswith("I-"):
+            if cur_tag is not None:
+                json_tags[cur_tag].append(token)
+            else:
+                tag_name = tag[2:]
+                json_tags[tag_name] = [token]
+                cur_tag = tag_name
+        else:
+            cur_tag = None
+    json_tags = {k: " ".join(v) for k, v in json_tags.items()}
+    return json_tags
+
+
+def extract_bert_labels_and_predictions(results: list[dict]) -> tuple[list[dict], list[dict]]:
+    labels, predictions = [], []
+    for result in results:
+        e_labels, e_predictions = {}, {}
+        for target in ["NonImmu", "CK7", "TTF-1", "CK20", "P40"]:
+            e_labels.update(tags_to_json(result[f"{target}_tags"], result["tokens"]))
+            e_predictions.update(tags_to_json(result[f"{target}_pred"], result["tokens"]))
+        labels.append(e_labels)
+        predictions.append(e_predictions)
+    return labels, predictions
+
+
+def extract_labels_and_predictions(file_name: str, model_type: str) -> tuple[list[dict], list[dict]]:
+    results = load_from_jsonl(file_name)
+    if model_type == "llama":
+        return extract_llama_labels_and_predictions(results)
+    elif model_type == "t5":
+        return extract_t5_labels_and_predictions(results)
+    elif model_type == "bert":
+        return extract_bert_labels_and_predictions(results)
+    else:
+        raise ValueError("Invalid model type")
+
+
+def fill_in_unknown(example: dict) -> dict:
+    for target in TARGETS:
+        if target not in example:
+            example[target] = "unknown"
+    return example
+
+
+def clear_text(text: str) -> str:
+    clean_text = text.replace("\n", " ").replace("\t", " ").replace("\r", " ")
+    clean_text = re.sub(r"\s+", " ", clean_text)
+    if text in ["(+)", "(-)", "(+", "(-", "+)", "-)"]:
+        clean_text = clean_text.replace("(", "").replace(")", "")
+    return clean_text
+
+
+def metrics(labels, preds, method="hard_match"):
     tp, num_labels, num_preds = 0, 0, 0
     for label, pred in zip(labels, preds):
-        if label != "unknown" and label == pred: # true positive
-            tp += 1 
+        if label != "unknown":
+            if method == "hard_match" and label == pred: # true positive
+                tp += 1
+            elif method == "soft_match" and pred in label: # true positive
+                tp += 1
         num_labels += 1 if label != "unknown" else 0
         num_preds += 1 if pred != "unknown" else 0
     precision = tp / num_preds if num_preds != 0 else 0
@@ -43,20 +126,27 @@ def metrics(labels, preds):
 
 def main():
     args = parse_args()
+    Path(args.outputs_dir).mkdir(parents=True, exist_ok=True)
 
     # Load predictions
-    results = load_from_jsonl(args.predictions_file)
-    labels = [result["label"] for result in results]
-    predictions = [result["predict"] for result in results]
+    labels, predictions = extract_labels_and_predictions(args.predictions_file, args.model_type)
+
+    # Clear text
+    labels = [{k: clear_text(v) for k, v in label.items()} for label in labels]
+    predictions = [{k: clear_text(v) for k, v in prediction.items()} for prediction in predictions]
+
+    # Fill in unknown
+    labels = [fill_in_unknown(label) for label in labels]
+    predictions = [fill_in_unknown(prediction) for prediction in predictions]
 
     # Compute metrics
-    targets = list(labels[0].keys())
     classification_report = []
     confusion_matrix = []
-    for target in targets:
+    for target in TARGETS:
         labels_target = [label[target] for label in labels]
         predictions_target = [pred[target] for pred in predictions]
-        metrics_dict = metrics(labels_target, predictions_target)
+        method = "soft_match" if target == "Htype" else "hard_match"
+        metrics_dict = metrics(labels_target, predictions_target, method)
         classification_report.append(
             [target, metrics_dict["precision"], metrics_dict["recall"], metrics_dict["f1"]]
         )
--- compute_metrics.sh
+++ compute_metrics.sh
@@ -1,19 +1,59 @@
 python compute_metrics.py \
-  --predictions_file ./outputs/LLaMA3-8B-Instruct/loraplus/predict/tmu/generated_predictions.jsonl \
-  --outputs_dir ./outputs/LLaMA3-8B-Instruct/loraplus/predict/tmu/
+  --predictions_file outputs/bert-base/predict/ntu/generated_predictions.jsonl \
+  --outputs_dir outputs/bert-base/predict/ntu \
+  --model_type bert
 
 python compute_metrics.py \
-  --predictions_file ./outputs/LLaMA3-8B-Instruct/loraplus/predict/ntu/generated_predictions.jsonl \
-  --outputs_dir ./outputs/LLaMA3-8B-Instruct/loraplus/predict/ntu/
+  --predictions_file outputs/bert-base/predict/tmu/generated_predictions.jsonl \
+  --outputs_dir outputs/bert-base/predict/tmu \
+  --model_type bert
+
+python compute_metrics.py \
+  --predictions_file outputs/clinical-bert/predict/ntu/generated_predictions.jsonl \
+  --outputs_dir outputs/clinical-bert/predict/ntu \
+  --model_type bert
+
+python compute_metrics.py \
+  --predictions_file outputs/clinical-bert/predict/tmu/generated_predictions.jsonl \
+  --outputs_dir outputs/clinical-bert/predict/tmu \
+  --model_type bert
+
+python compute_metrics.py \
+  --predictions_file outputs/gatortron-base/predict/ntu/generated_predictions.jsonl \
+  --outputs_dir outputs/gatortron-base/predict/ntu \
+  --model_type bert
+
+python compute_metrics.py \
+  --predictions_file outputs/gatortron-base/predict/tmu/generated_predictions.jsonl \
+  --outputs_dir outputs/gatortron-base/predict/tmu \
+  --model_type bert
+
+python compute_metrics.py \
+  --predictions_file outputs/t5-base/predict/ntu/generated_predictions.jsonl \
+  --outputs_dir outputs/t5-base/predict/ntu \
+  --model_type t5
+
+python compute_metrics.py \
+  --predictions_file outputs/t5-base/predict/tmu/generated_predictions.jsonl \
+  --outputs_dir outputs/t5-base/predict/tmu \
+  --model_type t5
+
+python compute_metrics.py \
+  --predictions_file outputs/clinical-t5-base/predict/ntu/generated_predictions.jsonl \
+  --outputs_dir outputs/clinical-t5-base/predict/ntu \
+  --model_type t5
+
+python compute_metrics.py \
+  --predictions_file outputs/clinical-t5-base/predict/tmu/generated_predictions.jsonl \
+  --outputs_dir outputs/clinical-t5-base/predict/tmu \
+  --model_type t5
+
+python compute_metrics.py \
+  --predictions_file ./outputs/LLaMA3-8B-Instruct/loraplus/predict/tmu/generated_predictions.jsonl \
+  --outputs_dir ./outputs/LLaMA3-8B-Instruct/loraplus/predict/tmu/ \
+  --model_type llama
 
 python compute_metrics.py \
   --predictions_file ./outputs/LLaMA3-8B-Instruct/loraplus/predict/ntu_manual_rewritten/generated_predictions.jsonl \
-  --outputs_dir ./outputs/LLaMA3-8B-Instruct/loraplus/predict/ntu_manual_rewritten/
-
-python compute_metrics.py \
-  --predictions_file ./outputs/LLaMA3-8B/loraplus/predict/tmu/generated_predictions.jsonl \
-  --outputs_dir ./outputs/LLaMA3-8B/loraplus/predict/tmu/
-
-python compute_metrics.py \
-  --predictions_file ./outputs/LLaMA3-8B/loraplus/predict/ntu/generated_predictions.jsonl \
-  --outputs_dir ./outputs/LLaMA3-8B/loraplus/predict/ntu/
+  --outputs_dir ./outputs/LLaMA3-8B-Instruct/loraplus/predict/ntu_manual_rewritten/ \
+  --model_type llama
