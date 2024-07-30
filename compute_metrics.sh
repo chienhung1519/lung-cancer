cfe11b0f45c47e6c953add10357a77a8fab72ea3
chienhung1519 <chienhung1519@gmail.com>
2024-07-30 08:34:17 +0000

    llama3.1 evaluation

--- compute_metrics.sh
+++ compute_metrics.sh
@@ -1,59 +1,69 @@
-python compute_metrics.py \
-  --predictions_file outputs/bert-base/predict/ntu/generated_predictions.jsonl \
-  --outputs_dir outputs/bert-base/predict/ntu \
-  --model_type bert
+# python compute_metrics.py \
+#   --predictions_file outputs/bert-base/predict/ntu/generated_predictions.jsonl \
+#   --outputs_dir outputs/bert-base/predict/ntu \
+#   --model_type bert
+
+# python compute_metrics.py \
+#   --predictions_file outputs/bert-base/predict/tmu/generated_predictions.jsonl \
+#   --outputs_dir outputs/bert-base/predict/tmu \
+#   --model_type bert
+
+# python compute_metrics.py \
+#   --predictions_file outputs/clinical-bert/predict/ntu/generated_predictions.jsonl \
+#   --outputs_dir outputs/clinical-bert/predict/ntu \
+#   --model_type bert
+
+# python compute_metrics.py \
+#   --predictions_file outputs/clinical-bert/predict/tmu/generated_predictions.jsonl \
+#   --outputs_dir outputs/clinical-bert/predict/tmu \
+#   --model_type bert
+
+# python compute_metrics.py \
+#   --predictions_file outputs/gatortron-base/predict/ntu/generated_predictions.jsonl \
+#   --outputs_dir outputs/gatortron-base/predict/ntu \
+#   --model_type bert
+
+# python compute_metrics.py \
+#   --predictions_file outputs/gatortron-base/predict/tmu/generated_predictions.jsonl \
+#   --outputs_dir outputs/gatortron-base/predict/tmu \
+#   --model_type bert
+
+# python compute_metrics.py \
+#   --predictions_file outputs/t5-base/predict/ntu/generated_predictions.jsonl \
+#   --outputs_dir outputs/t5-base/predict/ntu \
+#   --model_type t5
+
+# python compute_metrics.py \
+#   --predictions_file outputs/t5-base/predict/tmu/generated_predictions.jsonl \
+#   --outputs_dir outputs/t5-base/predict/tmu \
+#   --model_type t5
+
+# python compute_metrics.py \
+#   --predictions_file outputs/clinical-t5-base/predict/ntu/generated_predictions.jsonl \
+#   --outputs_dir outputs/clinical-t5-base/predict/ntu \
+#   --model_type t5
+
+# python compute_metrics.py \
+#   --predictions_file outputs/clinical-t5-base/predict/tmu/generated_predictions.jsonl \
+#   --outputs_dir outputs/clinical-t5-base/predict/tmu \
+#   --model_type t5
+
+# python compute_metrics.py \
+#   --predictions_file ./outputs/LLaMA3-8B-Instruct/loraplus/predict/tmu/generated_predictions.jsonl \
+#   --outputs_dir ./outputs/LLaMA3-8B-Instruct/loraplus/predict/tmu/ \
+#   --model_type llama
+
+# python compute_metrics.py \
+#   --predictions_file ./outputs/LLaMA3-8B-Instruct/loraplus/predict/ntu_manual_rewritten/generated_predictions.jsonl \
+#   --outputs_dir ./outputs/LLaMA3-8B-Instruct/loraplus/predict/ntu_manual_rewritten/ \
+#   --model_type llama
 
 python compute_metrics.py \
-  --predictions_file outputs/bert-base/predict/tmu/generated_predictions.jsonl \
-  --outputs_dir outputs/bert-base/predict/tmu \
-  --model_type bert
-
-python compute_metrics.py \
-  --predictions_file outputs/clinical-bert/predict/ntu/generated_predictions.jsonl \
-  --outputs_dir outputs/clinical-bert/predict/ntu \
-  --model_type bert
-
-python compute_metrics.py \
-  --predictions_file outputs/clinical-bert/predict/tmu/generated_predictions.jsonl \
-  --outputs_dir outputs/clinical-bert/predict/tmu \
-  --model_type bert
-
-python compute_metrics.py \
-  --predictions_file outputs/gatortron-base/predict/ntu/generated_predictions.jsonl \
-  --outputs_dir outputs/gatortron-base/predict/ntu \
-  --model_type bert
-
-python compute_metrics.py \
-  --predictions_file outputs/gatortron-base/predict/tmu/generated_predictions.jsonl \
-  --outputs_dir outputs/gatortron-base/predict/tmu \
-  --model_type bert
-
-python compute_metrics.py \
-  --predictions_file outputs/t5-base/predict/ntu/generated_predictions.jsonl \
-  --outputs_dir outputs/t5-base/predict/ntu \
-  --model_type t5
-
-python compute_metrics.py \
-  --predictions_file outputs/t5-base/predict/tmu/generated_predictions.jsonl \
-  --outputs_dir outputs/t5-base/predict/tmu \
-  --model_type t5
-
-python compute_metrics.py \
-  --predictions_file outputs/clinical-t5-base/predict/ntu/generated_predictions.jsonl \
-  --outputs_dir outputs/clinical-t5-base/predict/ntu \
-  --model_type t5
-
-python compute_metrics.py \
-  --predictions_file outputs/clinical-t5-base/predict/tmu/generated_predictions.jsonl \
-  --outputs_dir outputs/clinical-t5-base/predict/tmu \
-  --model_type t5
-
-python compute_metrics.py \
-  --predictions_file ./outputs/LLaMA3-8B-Instruct/loraplus/predict/tmu/generated_predictions.jsonl \
-  --outputs_dir ./outputs/LLaMA3-8B-Instruct/loraplus/predict/tmu/ \
+  --predictions_file outputs/Llmama-3.1-8B-Instruct/lora/predict_ntu/generated_predictions.jsonl \
+  --outputs_dir outputs/Llmama-3.1-8B-Instruct/lora/predict_ntu \
   --model_type llama
 
 python compute_metrics.py \
-  --predictions_file ./outputs/LLaMA3-8B-Instruct/loraplus/predict/ntu_manual_rewritten/generated_predictions.jsonl \
-  --outputs_dir ./outputs/LLaMA3-8B-Instruct/loraplus/predict/ntu_manual_rewritten/ \
+  --predictions_file outputs/Llmama-3.1-8B-Instruct/lora/predict_tmu/generated_predictions.jsonl \
+  --outputs_dir outputs/Llmama-3.1-8B-Instruct/lora/predict_tmu \
   --model_type llama
